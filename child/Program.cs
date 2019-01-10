004c61717c5dc1e70d953e87d18f61b686877dc7
Claudiu Guiman <clguiman@microsoft.com>
2019-01-10 09:33:35 -0800

    first commit

---
+++ child/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace child
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var pingStr = $"{DateTime.Now} Ping";
+                Console.WriteLine(pingStr);
+                File.AppendAllLines("log.txt", new string[] { pingStr });
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+            }
+        }
+    }
+}
---
+++ parentNetCore/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace parentNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "child.exe",
+                Arguments = "",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            var childProcess = new Process
+            {
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
+            };
+
+            var outputMonitoringCancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            childProcess.OutputDataReceived += ProcessDataHandler;
+            childProcess.ErrorDataReceived += ProcessDataHandler;
+
+
+            childProcess.Start();
+
+            childProcess.BeginOutputReadLine();
+            childProcess.BeginErrorReadLine();
+
+            var forget = Task.Run(async () =>
+            {
+                try
+                {
+                    await Task.Delay(-1, outputMonitoringCancellationTokenSource.Token).ConfigureAwait(false);
+                }
+                catch (TaskCanceledException)
+                {
+                }
+
+                Console.WriteLine("Cancel output read");
+
+                childProcess.CancelErrorRead();
+                childProcess.CancelOutputRead();
+            });
+
+            while (true)
+            {
+                while (_outputData.TryDequeue(out var line))
+                {
+                    Console.WriteLine(line);
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private static void ProcessDataHandler(object sender, DataReceivedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.Data))
+            {
+                return;
+            }
+            
+            _outputData.Enqueue(e.Data);
+
+        }
+
+        private static readonly ConcurrentQueue<string> _outputData = new ConcurrentQueue<string>();
+    }
+}
